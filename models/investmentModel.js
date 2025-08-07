ed01378617ca6ec15ad9d81fccd52c3a7e5bdb5c
Haritha R <1rn21cs117.r.haritha@gmail.com>
2025-08-07 10:12:53 +0000

    updates made on api

--- models/investmentModel.js
+++ models/investmentModel.js
@@ -1,8 +1,43 @@
 const db=require('./db');
 const Investment={
+
+    getOverallSummary: (callback) => {
+        const query = `
+            SELECT 
+                it.name AS category,
+                SUM(t.quantity * t.price_per_unit) AS total
+            FROM investments i
+            JOIN investment_types it ON i.type_id = it.type_id
+            JOIN transactions t ON t.investment_id = i.investment_id
+            WHERE t.txn_type = 'BUY'
+            GROUP BY it.name
+        `;
+        db.query(query, callback);
+    },
+
+    getPortfolioSummary: (callback) => {
+        const query = `
+            SELECT 
+                i.category,
+                SUM(t.units * t.price_at_time) AS invested_amount,
+                SUM(t.units * COALESCE(lp.current_price, 0)) AS current_value
+            FROM investments i
+            LEFT JOIN transactions t ON i.id = t.investment_id
+            LEFT JOIN (
+                SELECT investment_id, current_price
+                FROM live_prices
+                GROUP BY investment_id
+            ) lp ON i.id = lp.investment_id
+            GROUP BY i.category;
+        `;
+        db.query(query, callback);
+    },
+    
+    
     getAll: (callback) =>{
         db.query('SELECT * FROM investments',callback);
     },
+    
     getByid: (id, callback) =>{
         db.query('SELECT * FROM investments WHERE type_id = ?', [id], callback);
     },
--- routes/investmentRoutes.js
+++ routes/investmentRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 let controller= require('../controller/investmentController');
 
-router.get("/:id",controller.getInvestmentById)
-router.get("/",controller.getAllInvestments)
-router.post("/",controller.addInvestment)
-router.put("/:id",controller.updateInvestment)
+router.get("/summary", controller.getOverallInvestments);
 router.get("/types", controller.getTypes);
 router.get("/subcategories/:typeId", controller.getSubcategories);
+router.get("/:id", controller.getInvestmentById);
+router.get("/", controller.getAllInvestments);
+router.post("/", controller.addInvestment);
+router.put("/:id", controller.updateInvestment);
+
 
 module.exports = router
